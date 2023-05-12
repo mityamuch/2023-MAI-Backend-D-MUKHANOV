38a0dd33212b23cce2f99a96ddc7315c8562f3a3
dmitriy mukhanov <74504728+mityamuch@users.noreply.github.com>
2023-05-12 15:35:13 +0300

    LruCache remake

--- LRUCache.py
+++ LRUCache.py
@@ -1,40 +1,64 @@
+class Node:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.prev = None
+        self.next = None
+
+
 class LRUCache:
-    def __init__(self, capacity: int=10) -> None:
+    def __init__(self, capacity):
         self.capacity = capacity
-        self.cache = {}
-        self.key_order = []
-    
-    def get(self, key: str) -> str:
-        if key in self.cache:
-            self.key_order.remove(key)
-            self.key_order.append(key)
-            return self.cache[key]
-        else:
-            return None
+        self.dict = {}
+        self.head = Node(0, 0)
+        self.tail = Node(0, 0)
+        self.head.next = self.tail
+        self.tail.prev = self.head
 
-    def set(self, key: str, value: str) -> None:
-        if key in self.cache:
-            self.key_order.remove(key)
-            self.key_order.append(key)
-        else:
-            self.key_order.append(key)
-        self.cache[key] = value
-        if len(self.key_order) > self.capacity:
-            oldest_key = self.key_order.pop(0)
-            del self.cache[oldest_key]
+    def get(self, key):
+        if key in self.dict:
+            node = self.dict[key]
+            self._remove(node)
+            self._add(node)
+            return node.value
+        return -1
 
-    def rem(self, key: str) -> None:
-        if key in self.cache:
-            self.key_order.remove(key)
-            del self.cache[key]
+    def _add(self, node):
+        p = self.tail.prev
+        p.next = node
+        self.tail.prev = node
+        node.prev = p
+        node.next = self.tail
+
+    def _remove(self, node):
+        p = node.prev
+        n = node.next
+        p.next = n
+        n.prev = p
+
+    def set(self, key, value):
+        if key in self.dict:
+            self._remove(self.dict[key])
+        node = Node(key, value)
+        self._add(node)
+        self.dict[key] = node
+        if len(self.dict) > self.capacity:
+            node = self.head.next
+            self._remove(node)
+            del self.dict[node.key]
+
+    def rem(self, key):
+        if key in self.dict:
+            node = self.dict[key]
+            self._remove(node)
+            del self.dict[key]
 
 
-
-cache = LRUCache()
+cache = LRUCache(10)
 cache.set('Jesse', 'Pinkman')
 cache.set('Walter', 'White')
 cache.set('Jesse', 'James')
 
-print(cache.get('Jesse')) # вернёт 'James'
+print(cache.get('Jesse'))  # вернёт 'James'
 cache.rem('Walter')
-print(cache.get('Walter')) # вернёт ''
+print(cache.get('Walter'))  # вернёт '-1'
